cd931211d9dcab5ba2a36f59fa30feebe8382632
Sunil  Kunkekar <sunilkunkekar@percipient.in>
2024-09-04 23:27:04 +0530

    Add the odd or even number program

---
+++ Odd_even_number.py
@@ -0,0 +1,19 @@
+# num = int(input("Enter the Number :"))
+# if num % 2==0:
+#     print(num,"is even number")
+# else:
+#     print(num,"is Odd number")
+    
+#=============================================
+#using the function :
+
+def check_odd_even_number():
+    num = int(input("Enter the Number:"))
+    if num % 2 ==0:
+        print(num,"is an even number")
+    else:
+        print(num,"is an odd number")
+
+check_odd_even_number()
+#=============================================
+
